5f01e32101a384045e726c39f91676fbebc975e1
hy chang <hy.chang@hotmail.com>
2014-11-01 01:13:49 +0800

    First Add MVC WEB

---
+++ P1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(P1.Startup))]
+namespace P1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
